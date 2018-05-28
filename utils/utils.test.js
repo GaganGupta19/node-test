d68f3720960d8503cec6ca6d457a6bcf40ff8cc3
Gagan <gagan_gupta@live.in>
2018-05-28 23:39:09 +0530

    using assertions using expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,21 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     let res = utils.add(1, 3);
-    if(res !== 4){
-        throw new Error('Value not correct.');
-    }
+    expect(res).toBe(4).toBeA('number');
 });
 
 it('should square the number', () => {
     let res = utils.square(2);
-    if(res !== 4){
-        throw new Error('Value not correct.');
-    }
+    expect(res).toBe(4).toBeA('number');
+});
+
+it('should set names', () => {
+    let user = {location: 'Ashok Vihar', age: 25};
+    let res = utils.setName(user, 'Gagan Gupta');
+    expect(res).toInclude({
+        firstName: 'Gagan',
+        lastName: 'Gupta'
+    })
 });
